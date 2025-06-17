447a7222838843725a6e3b8561dbf68e47aecac1
Nawaz Khurshid <nawaz1981@gmail.com>
2025-06-17 12:23:23 +0200

    Fixes for pipeline

--- src/launch.sh
+++ src/launch.sh
@@ -9,11 +9,11 @@ export GDAL_DATA=/home/nonroot/miniforge3/share/gdal
 export GDAL_DRIVER_PATH=/home/nonroot/miniforge3/lib/gdalplugins
 export PROJ_DATA=/home/nonroot/miniforge3/share/proj
 cd /app
-echo "{'s1_ascending': '$1', 's1_descending': '$2', 'start':'$3', 'end':'$4', 'outputArtifactName': '$5'}"
+echo "{'s1_ascending': '$1', 's1_descending': '$2', 'startDate':'$3', 'endDate':'$4', 'outputArtifactName': '$5'}"
 #export PATH="/home/nonroot/miniforge3/snap/.snap/auxdata/gdal/gdal-3-0-0/bin/:$PATH"
 echo "GDAL DATA AFTER EXPORT:"
 echo $GDAL_DATA
 echo "PROJ_LIB AFTER EXPORT"
 echo $PROJ_LIB
-python main.py "{'s1_ascending': '$1', 's1_descending': '$2', 'start':'$3', 'end':'$4', 'outputArtifactName': '$5'}"
+python main.py "{'s1_ascending': '$1', 's1_descending': '$2', 'startDate':'$3', 'endDate':'$4', 'outputArtifactName': '$5'}"
 exit
