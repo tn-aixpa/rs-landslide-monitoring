3c3ce3c56ecc74366c4f5b60a09bf3e83e328299
lucabergamasco <lbergamasco@fbk.eu>
2026-08-18 16:48:02 +0200

    Update job_insar_preprocessing.py

--- src/inference/job_insar_preprocessing.py
+++ src/inference/job_insar_preprocessing.py
@@ -8,7 +8,7 @@ import tempfile
 import numpy as np
 import shutil
 import digitalhub as dh
-from utils.sdk_handler import upload_artifact
+from utils.skd_handler import upload_artifact
 from osgeo import gdal
 gdal.UseExceptions()
 import subprocess
